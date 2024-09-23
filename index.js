74f9dd02ca9910c54126e5805b0a288395b532b6
vishalbanderwale <vishalsonu945@gmail.com>
2024-09-23 23:54:16 +0530

    variables

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let a = 10;
-let b = a;
+let b = 5;
+let c = false;
+let d = undefined;
+let f = null;
 console.log(b + a);
 let res = a * b;
 console.log(res);
 
 let nameFirst = "vishal";
+nameFirst = "singh";
 console.log(nameFirst);
 
-// Allow use let for better practice
+//variables: are used to store information ex:let,const,var
+// Always use let for better practice ES6 before we use to have var
 //if we dont decalre value to variable than its  undefined
 // for variable follow camel case notation  for best practice: firstletter of first word lowercase than uppercase for second word,third...
+// variable declartion syntax : variable variable name; than initalize a variable : var var name=value : let a=10;
+// In let we can we reintialze a value but in constant it will throw an error
+// primitive data types/value types : numbers,string, booelan, null,undefined
+// Non primitive data types : objects,array,function
