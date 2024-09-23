8f6558e90bfbb9b7d96714ea943353e559bd84b2
vishalbanderwale <vishalsonu945@gmail.com>
2024-09-23 22:58:17 +0530

    let

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let a = 10;
 let b = a;
-console.log(b+a);
-var res=a*b;
+console.log(b + a);
+let res = a * b;
 console.log(res);
+
+let nameFirst = "vishal";
+console.log(nameFirst);
+
+// Allow use let for better practice
+//if we dont decalre value to variable than its  undefined
+// for variable follow camel case notation  for best practice: firstletter of first word lowercase than uppercase for second word,third...
