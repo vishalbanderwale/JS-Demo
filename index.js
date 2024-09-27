8825899673634a5ad0816b6cf863dc79431f98ce
vishalbanderwale <vishalsonu945@gmail.com>
2024-09-27 19:14:09 +0530

    hoisting

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ let nameFirst = "vishal";
 nameFirst = "singh";
 console.log(nameFirst);
 
-//variables: are used to store information ex:let,const,var
+//hoisting
+console.log(result);
+var result = "name";
+
+//objects
+
+let obj1 = { a: 1, b: 2 };
+obj1.a = "singh";
+console.log(obj1);
+
+// arrays
+// last element of array: array[array.length-1]
+
+let array1 = [1, 2, 3, 4, 5];
+array1.push(8);
+console.log(array1.length);
+
+//variables: are used to store information(data value) ex:let,const,var
 // Always use let for better practice ES6 before we use to have var
 //if we dont decalre value to variable than its  undefined
 // for variable follow camel case notation  for best practice: firstletter of first word lowercase than uppercase for second word,third...
-// variable declartion syntax : variable variable name; than initalize a variable : var var name=value : let a=10;
-// In let we can we reintialze a value but in constant it will throw an error
+// 3 types of variable in js : let,const,var
+// variable declartion syntax : keyword(let,const,var) variable name(also know as identifiers): let a; than initalize a variable : keyword var name=value : let a=10;
+// In let we can  reintialze a value but in constant it will throw an error
 // primitive data types/value types : numbers,string, booelan, null,undefined
 // Non primitive data types : objects,array,function
+// let and const are block scope(only can be accessed inside block) var is functional scope
+//hoisting: var variables are hoisted at the top we can access before declartion only decalrtion are hosited at top so value is undefined
+// in var we get result: undefined , for let ,const : reference error
